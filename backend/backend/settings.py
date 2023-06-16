2e7617ee5b4073f8fbaec27b89d15bfdd339a37a
Игорь Фокин <withworld@yandex.ru>
2023-06-16 17:22:55 +0300

    change setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'fokin.hopto.org',
+    '158.160.14.104',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
